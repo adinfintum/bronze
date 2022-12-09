9bfd74dd639e34e7690d12c8e0ef144c30092057
bronze <bandoomillion@gmail.com>
2022-12-08 20:52:51 -0500

    update

---
+++ loghistory.sh
@@ -0,0 +1,13 @@
+#/bin/bash
+
+#Script: Ops 201 Class 02 Ops Challenge Solution
+#Author: Adam
+#Date of latest revision: 12/7/22
+#Purpose: Print a string to the terminal
+#echo "Here is where i would write my code"
+
+
+function log(){
+    last
+};
+log
