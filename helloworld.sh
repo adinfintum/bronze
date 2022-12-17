8e9c0c12181523132287a7530394012c285db3d7
bronze <bandoomillion@gmail.com>
2022-12-17 12:10:21 -0500

    ops challenge2

---
+++ helloworld.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Script: Ops 201 Class 02 Ops Challenge Solution
+# Author: ADAM BANDOO
+# Date of latest revision: 17DEC22
+# Purpose: Print a string to the terminal
+
+
+# Main
+
+echo "i am working perfectly"
+
+# End
