8508354688d524cb46357e43bceedd6b5d2e82f5
bronze <bandoomillion@gmail.com>
2022-12-17 12:50:43 -0500

    op challenge5

--- helloworld.sh
+++ helloworld.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Script: Ops 201 Class 02 Ops Challenge Solution
+# Script: Ops 201 Class 05 Ops Challenge Solution
 # Author: ADAM BANDOO
 # Date of latest revision: 17DEC22
 # Purpose: Print a string to the terminal
@@ -8,6 +8,12 @@
 
 # Main
 
-echo "i am working perfectly"
+for m in mines
+do
+ps aux
+echo "enter a pid"
+read pid 
+kill $pid
+done
 
 # End
