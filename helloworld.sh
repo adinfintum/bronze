93707756b40de99d3c71ddf9786b1b37d3232301
bronze <bandoomillion@gmail.com>
2023-01-05 20:04:01 -0500

    ops challenge12

--- helloworld.sh
+++ helloworld.sh
@@ -1,14 +1,8 @@
-Task1
-Get-Process | Sort-Obejct CPU -Descending
-Task2
-Get-Process | Sort Object Id -Descending
-Task3
-Get-Process | Sort-Obejct WS -Desecennding | Select-Object -First 5
-Task4
-Start-Process -FIlepath "C:\Program FIles\Internet Explorer\iexplore.exe" https//owasp.org/www-project-top-ten/
-Task5
-for ($i = 0; $i -lt 10; $i++) {Start-Process "C:\Program FIles\Internet Explorer\iexplore.exe" https://owasp.org/www-project-top-ten/}
-Task6
-Get-Process iexplore | Stop-Process
-Task7
-Stop-Process -Id 1464 -Force
+Task1: Create a local file called network_report.txt
+ipconfig /all | Out-file -FilePath C:\Users\cyber\Desktop\network_report.txt
+
+Task2: Use Select-String to search network_report.txt and return only the IP version 4 address.
+Select-String -Path C:\Users\cyber\Desktop\network_report.txt -Pattern IPv4
+
+Task3:Remove the network_report.txt 
+Remove-Item -Path "C:\Users\cyber\Desktop\network_report.txt"
