a98fa597b9ea42e72c5d5276b1a6c29e809c2faa
bronze <bandoomillion@gmail.com>
2023-02-24 20:45:46 -0500

    301ops8.py

---
+++ 301ops7.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+# Import libraries
+
+import os
+
+# Declaration of variables
+
+### Read user input here into a variable
+filepath = input("Enter file path:")
+
+# Declaration of functions
+
+### Declare a function here
+
+for (root, dirs, files) in os.walk("testdir"):
+    ### Add a print command here to print ==root==
+    print(root)
+    ### Add a print command here to print ==dirs==
+    print(dirs)
+    ### Add a print command here to print ==files==
+    print(files)
+
+# Main
+
+### Pass the variable into the function here
+
+# End
---
+++ 301ops8.py
@@ -0,0 +1,11 @@
+fruits = ["apple", "banana", "cherry", "date", "elderberry", "fig", "grape", "strawberry", "watermelon", "orange"]
+
+# Print the fourth element
+print(fruits[3])
+
+# Print the sixth through tenth elements
+print(fruits[5:])
+
+# Change the value of the seventh element
+fruits[6] = "onion"
+print(fruits)
