998cd245df5c90f64d36738effc52a37004fa7c7
bronze <bandoomillion@gmail.com>
2023-02-26 02:21:24 -0500

    301ops9.py

---
+++ 301ops9.py
@@ -0,0 +1,41 @@
+# Create if statements using these logical conditionals below. Each statement should print information to the screen depending on if the condition is met.
+a = 5
+b = 10
+
+if a == b:
+    print("a equals b")
+
+if a != b:
+    print("a does not equal b")
+
+if a < b:
+    print("a is less than b")
+
+if a <= b:
+    print("a is less than or equal to b")
+
+if a > b:
+    print("a is greater than b")
+
+if a >= b:
+    print("a is greater than or equal to b")
+
+# Create an if statement using a logical conditional of your choice and include elif keyword that executes when other conditions are not met.
+x = 10
+
+if x > 10:
+    print("x is greater than 10")
+elif x < 10:
+    print("x is less than 10")
+else:
+    print("x is equal to 10")
+
+# Create an if statement that includes both elif and else to execute when both if and elif are not met.
+x = 5
+
+if x < 5:
+    print("x is less than 5")
+elif x == 5:
+    print("x is equal to 5")
+else:
+    print("x is greater than 5")
